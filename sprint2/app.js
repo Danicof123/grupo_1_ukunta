e1379a4012722952a3f8befcf8f3142eee22e45d
Danicof <danicoof@gmail.com>
2021-06-30 20:31:15 -0300

    Se agrego la ruta nosotros

--- sprint2/app.js
+++ sprint2/app.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
 app.get('/404', (req,res) => res.sendFile(path.join(__dirname, 'views', '404.html')));
 app.get('/grid', (req,res) => res.sendFile(path.join(__dirname, 'views', 'test', 'grid-test.html'))); //Eliminar esta linea - solo para testeo
-app.get('/about', (req,res) => res.sendFile(path.join(__dirname, 'views', 'about.html')));
+app.get('/nosotros', (req,res) => res.sendFile(path.join(__dirname, 'views', 'about.html')));
 app.get('/cart', (req,res) => res.sendFile(path.join(__dirname, 'views', 'cart.html')));
 app.get('/login', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
 app.get('/products', (req,res) => res.sendFile(path.join(__dirname, 'views', 'products.html')));
