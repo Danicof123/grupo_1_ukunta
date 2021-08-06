e7f99e4e655a75a0b87628e398956e89b9795063
Danicof123 <danicoof@gmail.com>
2021-08-06 12:22:22 -0300

    drop menu user

---
+++ sitio/public/js/main.js
@@ -0,0 +1,9 @@
+import dropMenu from "./modules/drop-menu.js";
+
+((c,d,w)=>{
+
+
+    d.addEventListener('DOMContentLoaded', e => {
+        dropMenu(".btn-drop") //Cuando le hago click al boton
+    })
+})(console, document, window);
