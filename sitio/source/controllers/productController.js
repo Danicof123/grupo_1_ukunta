9295413cf68ae304e19cea3eb891e3258ea192b6
Emanuel Romero <e.romero.1989@gmail.com>
2021-08-10 00:14:38 -0300

    Agrego validator y multer a add y edit products

--- sitio/source/controllers/productController.js
+++ sitio/source/controllers/productController.js
@@ -1,47 +1,47 @@
-let { productos_db, guardarProducto } = require('../data/products_db');
+const {validationResult} = require('express-validator');
+const products_db = require('../data/products_db');
 
+let {productos_db, guardarProducto} = require('../data/products_db');
 
-const comprobarId = id => (id && !!productos_db.find(e => e.id === id));
+const comprobarId = (id) => id && !!productos_db.find((e) => e.id === id);
 
-const reemplazarProd = obj => {
-   productos_db.forEach(pr => {
+const reemplazarProd = (obj) => {
+   productos_db.forEach((pr) => {
       if (pr.id === obj.id) {
-         pr.name = obj.name
-         pr.category = obj.category
-         pr.description = obj.description
-         pr.price = obj.price
-         pr.image = obj.image || pr.image
+         pr.name = obj.name;
+         pr.category = obj.category;
+         pr.description = obj.description;
+         pr.price = obj.price;
+         pr.image = obj.image || pr.image;
          return;
       }
    });
-}
+};
 
-const eliminarProd = id => productos_db = productos_db.filter(pr => pr.id !== id);
+const eliminarProd = (id) => (productos_db = productos_db.filter((pr) => pr.id !== id));
 
-const crearProd = obj => {
+const crearProd = (obj) => {
    obj.id = productos_db[productos_db.length - 1].id + 1;
-   obj.image = obj.image || "not.jpg"
+   obj.image = obj.image || 'not.jpg';
    productos_db.push(obj);
-}
+};
 
 const escribirBD = () => {
    guardarProducto(productos_db);
-}
+};
 
 module.exports = {
-
-
    store: (req, res) => {
       let productos;
       switch (req.params.cat) {
-         case "cerveza":
-            productos = productos_db.filter(pr => pr.category === "cerveza");
+         case 'cerveza':
+            productos = productos_db.filter((pr) => pr.category === 'cerveza');
             break;
-         case "hidromiel":
-            productos = productos_db.filter(pr => pr.category === "hidromiel");
+         case 'hidromiel':
+            productos = productos_db.filter((pr) => pr.category === 'hidromiel');
             break;
-         case "merchandising":
-            productos = productos_db.filter(pr => pr.category === "merchandising");
+         case 'merchandising':
+            productos = productos_db.filter((pr) => pr.category === 'merchandising');
             break;
          default:
             productos = productos_db;
@@ -50,67 +50,90 @@ module.exports = {
       const locals = {
          title: 'Tienda Ukunta',
          productos_db: productos,
-      }
+      };
       res.render('store', locals);
    },
 
    detail: (req, res) => {
       const id = parseInt(req.params.id, 10);
 
-      if (!comprobarId(id))
-         res.redirect('/store');
+      if (!comprobarId(id)) res.redirect('/store');
 
       let producto = productos_db.find((producto) => producto.id === id);
+      let prRelations = productos_db.filter(pr => pr.category === producto.category && pr.id !=producto.id);
 
       res.render('products', {
          title: 'Detalle Producto',
          producto,
          productos_db,
+         prRelations,
       });
    },
    addProduct: (req, res) => {
-
       res.render('addProduct', {
          title: 'Agregar producto',
          productos_db,
       });
    },
    createProduct: (req, res) => {
-      crearProd(req.body);
-      escribirBD();
-      res.redirect('/store');
+      const resultValidation = validationResult(req);
+
+      if (resultValidation.errors.length > 0) {
+         return res.render('addProduct', {
+            errors: resultValidation.mapped(),
+            old: req.body,
+            title: 'Agregar producto',
+            productos_db,
+         });
+      } else {
+         crearProd(req.body);
+         escribirBD();
+         res.redirect('/store');
+      }
    },
 
    editProducto: (req, res) => {
       const id = parseInt(req.params.id, 10);
 
-      if (!comprobarId(id))
-         res.redirect('/store');
+      if (!comprobarId(id)) res.redirect('/store');
       const locals = {
-         title: "Editando ",
-         product: productos_db.find(pr => pr.id === +req.params.id)
-      }
+         title: 'Editando ',
+         product: productos_db.find((pr) => pr.id === +req.params.id),
+      };
       res.render('editProduct', locals);
    },
-
 
    updateProducto: (req, res) => {
       const id = parseInt(req.body.id, 10);
 
-      if (comprobarId(id)) {
-         const pr = req.body; //Guarda el producto editado en pr
-         pr.id = id;
-         reemplazarProd(pr)
-         escribirBD()
-         res.redirect('/store');
+      const resultValidation = validationResult(req);
+
+      if (resultValidation.errors.length > 0) {
+         if (comprobarId(id)) {
+            return res.render('editProduct', {
+               errors: resultValidation.mapped(),
+               old: req.body,
+               title: 'Editando ',
+               product: productos_db.find((pr) => pr.id === +req.params.id),
+            });
+         }
+      } else {
+         if (comprobarId(id)) {
+            const pr = req.body; //Guarda el producto editado en pr
+            pr.id = id;
+            reemplazarProd(pr);
+            escribirBD();
+            res.redirect(`/store/products/${req.params.id}`);
+         }
       }
    },
+
    remove: (req, res) => {
       const id = parseInt(req.params.id, 10);
-      if (comprobarId(id)){
-         eliminarProd(id)
-         escribirBD()
+      if (comprobarId(id)) {
+         eliminarProd(id);
+         escribirBD();
       }
       res.redirect('/store');
-   }
+   },
 };
--- sitio/source/routes/products.js
+++ sitio/source/routes/products.js
@@ -1,17 +1,53 @@
 const express = require('express'),
    router = express.Router(),
+   path = require('path'),
+   multer = require('multer'),
+   {body} = require('express-validator'),
+   validations = [
+      body('name').notEmpty().withMessage('Debes ingresar el nombre del producto.'),
+      body('category').notEmpty().withMessage('Debes elegir la categoría.'),
+      body('description').notEmpty().withMessage('Debes ingresar la descripción del producto.'),
+      body('price').notEmpty().withMessage('Debes ingresar el precio del producto.'),
+      body('images').custom((value, {req}) => {
+         let file = req.file;
+         let aceptedExtensions = ['.jpg', '.jpeg', '.png'];
+
+         if (!file) {
+            throw new Error('Debes seleccionar al menos una imagen para tu producto');
+         } else {
+            let fileExtension = path.extname(file.originalname);
+            if (!aceptedExtensions.includes(fileExtension)) {
+               throw new Error(`La imagen debe ser de tipo: ${aceptedExtensions.join(', ')}.`);
+            }
+         }
+         return true;
+      }),
+   ],
+   validationsEdit = [
+      body('name').notEmpty().withMessage('Debes ingresar el nombreo modificar el actual.'),
+      body('description').notEmpty().withMessage('Debes ingresar una descripción o dejar la actual.'),
+      body('price').notEmpty().withMessage('Debes ingresar el nuevo precio o dejar el actual.'),
+   ],
+   storage = multer.diskStorage({
+      destination: (req, file, cb) => {
+         cb(null, './public/images/products/addProducts');
+      },
+      filename: (req, file, cb) => {
+         let fileName = `${Date.now()}_img${path.extname(file.originalname)}`;
+         cb(null, fileName);
+      },
+   }),
+   uploadFile = multer({storage}),
    // Controllers
    {store, detail, addProduct, editProducto, updateProducto, remove, createProduct} = require('../controllers/productController');
 
 /* /store */
 router.get('/:cat?', store);
 router.get('/products/add', addProduct);
+router.post('/', uploadFile.single('images'), validations, createProduct);
 router.get('/products/:id', detail);
-router.post('/', createProduct)
-router.get('/edit/:id', editProducto);
-router.put('/', updateProducto);
+router.get('/products/edit/:id', editProducto);
+router.put('/products/edit/:id', validationsEdit, updateProducto);
 router.delete('/delete/:id', remove);
-
-
 
 module.exports = router;
