9d06017ee979228590fc6627878347a98e6c2cb8
B2DF <bernardinarte@gmail.com>
2021-08-11 22:44:52 -0300

    creacion de middleware, validaciones y rutas de registro de usuario

--- sitio/source/routes/users.js
+++ sitio/source/routes/users.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { usersValidator } = require('../modules/validator')
 
-const {login, register} = require('../controllers/userController');
+const {login, register, createUser} = require('../controllers/userController');
 
 router.get('/login', login);
 router.get('/register', register);
+router.post('/register', usersValidator , createUser)
 
 
 module.exports = router;
