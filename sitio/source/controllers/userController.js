eab23cbd29d38928f99cef66530717b4b06a6b0e
Danicof123 <danicoof@gmail.com>
2021-08-12 14:37:11 -0300

    FixDevelop

--- sitio/source/controllers/userController.js
+++ sitio/source/controllers/userController.js
@@ -1,13 +1,70 @@
+const {users_db, guardarUser} = require('../data/users_db');
+const {validationResult} = require('express-validator')
+
+const userCreation = user => {
+    users_db.push(user);
+};
+
+const saveUser = () => {
+    guardarUser(users_db)
+};
+
+
+
 module.exports = {
     login: (req,res) => {
         res.render('login', {
             title : 'Iniciar sesión',
         })
     },
+
     register : (req,res) => {
+        
+      
         res.render('register', {
             title : 'Registrese',
+            users_db
         })
+    },
+    
+    createUser: (req,res) => {
+        let {name, lastname, datebirth, DNI, phone, email, password, country, state, city, adress} = req.body;
+        let errors = validationResult(req);
+
+        let locals = {
+                title : 'Registrese',
+                errors : errors.mapped(),
+                old : req.body,
+                users_db
+        };
+        
+        if(errors.isEmpty()){
+
+            let user = {
+                name,
+                lastname, 
+                datebirth,
+                DNI,
+                phone,
+                email,
+                password,
+                country,
+                state,
+                city,
+                adress,
+            }
+
+            userCreation(user);
+            saveUser();
+            res.redirect('/');
+
+        } else {
+            
+            
+            res.render('register', locals)
+        }
+        
+        
     }
     
-}
+}   
