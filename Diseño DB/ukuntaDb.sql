a8631727617db1f4b18b900039484c5fc25d93e4
Emanuel Romero <e.romero.1989@gmail.com>
2021-09-17 00:50:17 -0300

    Se corrige nombre de tablas

--- Diseño DB/ukuntaDb.sql
+++ Diseño DB/ukuntaDb.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `ukuntadb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `ukuntadb`;
--- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: ukuntadb
+-- Host: localhost    Database: ukuntadb
 -- ------------------------------------------------------
--- Server version	8.0.25
+-- Server version	8.0.26
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,13 +18,13 @@ USE `ukuntadb`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `address`
+-- Table structure for table `addresses`
 --
 
-DROP TABLE IF EXISTS `address`;
+DROP TABLE IF EXISTS `addresses`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `address` (
+CREATE TABLE `addresses` (
   `id` int NOT NULL AUTO_INCREMENT,
   `street` varchar(100) NOT NULL,
   `streetNumber` int NOT NULL,
@@ -40,12 +40,12 @@ CREATE TABLE `address` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `address`
+-- Dumping data for table `addresses`
 --
 
-LOCK TABLES `address` WRITE;
-/*!40000 ALTER TABLE `address` DISABLE KEYS */;
-/*!40000 ALTER TABLE `address` ENABLE KEYS */;
+LOCK TABLES `addresses` WRITE;
+/*!40000 ALTER TABLE `addresses` DISABLE KEYS */;
+/*!40000 ALTER TABLE `addresses` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -94,7 +94,7 @@ CREATE TABLE `cartproduct` (
   PRIMARY KEY (`id`),
   KEY `FK_79be3f3c-4cd8-46bb-ab21-ec6c26f86d1e` (`productId`),
   KEY `FK_d50a75b8-9c70-472f-a278-af894c1cdff1` (`cartId`),
-  CONSTRAINT `FK_79be3f3c-4cd8-46bb-ab21-ec6c26f86d1e` FOREIGN KEY (`productId`) REFERENCES `product` (`id`),
+  CONSTRAINT `FK_79be3f3c-4cd8-46bb-ab21-ec6c26f86d1e` FOREIGN KEY (`productId`) REFERENCES `products` (`id`),
   CONSTRAINT `FK_d50a75b8-9c70-472f-a278-af894c1cdff1` FOREIGN KEY (`cartId`) REFERENCES `cart` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -144,7 +144,7 @@ CREATE TABLE `images` (
   `productId` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_003eafc3-efcd-4ba8-a3e2-1b7552dd5e3d` (`productId`),
-  CONSTRAINT `FK_003eafc3-efcd-4ba8-a3e2-1b7552dd5e3d` FOREIGN KEY (`productId`) REFERENCES `product` (`id`)
+  CONSTRAINT `FK_003eafc3-efcd-4ba8-a3e2-1b7552dd5e3d` FOREIGN KEY (`productId`) REFERENCES `products` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -172,10 +172,10 @@ CREATE TABLE `payments` (
   `expireDate` date NOT NULL,
   `securityCode` int NOT NULL,
   `dni` int NOT NULL,
-  `userID` int NOT NULL,
+  `userId` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK_8a3fc591-9564-434c-8071-777adf443f6f` (`userID`),
-  CONSTRAINT `FK_8a3fc591-9564-434c-8071-777adf443f6f` FOREIGN KEY (`userID`) REFERENCES `users` (`id`)
+  KEY `FK_8a3fc591-9564-434c-8071-777adf443f6f` (`userId`),
+  CONSTRAINT `FK_8a3fc591-9564-434c-8071-777adf443f6f` FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -189,13 +189,13 @@ LOCK TABLES `payments` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `product`
+-- Table structure for table `products`
 --
 
-DROP TABLE IF EXISTS `product`;
+DROP TABLE IF EXISTS `products`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `product` (
+CREATE TABLE `products` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   `description` varchar(255) NOT NULL,
@@ -211,12 +211,12 @@ CREATE TABLE `product` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `product`
+-- Dumping data for table `products`
 --
 
-LOCK TABLES `product` WRITE;
-/*!40000 ALTER TABLE `product` DISABLE KEYS */;
-/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -259,4 +259,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-09-15 23:41:51
+-- Dump completed on 2021-09-17  0:48:40
