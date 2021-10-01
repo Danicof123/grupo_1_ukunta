197bba5e0c9bfa51eb4a9c62177e924449b25640
Daniel Fernandez <dani@bebot.design>
2021-10-01 19:11:01 -0300

    Se agrego search, update y delete api producto

--- sitio/source/controllers/api/productController.js
+++ sitio/source/controllers/api/productController.js
@@ -1,6 +1,8 @@
 //DB
 const db = require('../../database/models');
+const {Op} = require('sequelize');
 
+// API
 const findAllProduct = async (req, res) => {
   res.json(await db.Product.findAll({
     include: ["category", "image"]
@@ -8,9 +10,11 @@ const findAllProduct = async (req, res) => {
 }
 
 const findById = async (req, res) => {
-  res.json(await db.Product.findByPk(req.params.id,{
+  const producto = await db.Product.findByPk(req.params.id,{
     include: ["category", "image"]
-  }))
+  })
+  if(producto) res.json(producto)
+  else res.status(404).json({"error": 404, "msg": "El producto no existe"})
 }
 
 const findByCategory = async (req, res) => {
@@ -21,14 +25,80 @@ const findByCategory = async (req, res) => {
   }))
 }
 
-// const findByTag = async (req, res) => {
-//   await
-// }
+const findByTag = async (req, res) => {
+  const find = req.query.s; //Palabra clave a buscar
+  // Traer la categoría q matchee (Luego uso el id)
+  const cat = await db.Category.findOne({where: {"name" : find}})
+
+  res.json(await db.Product.findAll({
+    // Si coincide nombre, description o categoria
+    where: {
+      [Op.or]: [
+          {name: { [Op.substring]: find }},
+          {description: { [Op.substring]: find}},
+          {categoryId: cat.id},
+      ]
+    } 
+  }))
+}
+
+// Actualizando el producto
+const updateProduct = async (req, res) => {
+  try{
+    const id = req.params.id;
+    const producto = await db.Product.findByPk(id)
+    // El producto que intento actualizar existe en la BD?
+    if(producto){
+      // Si no actualiza un dato dejo el que estaba
+      await db.Product.update({
+        name: req.body.name || producto.name,
+        description: req.body.description || producto.description,
+        size: req.body.size || producto.size,
+        price: req.body.price || producto.price,
+        stock: req.body.stock || producto.stock,
+        expire: req.body.expire || producto.expire,
+        categoryId: req.body.categoryId || producto.categoryId,
+      },
+      {where: {id : id}})
+
+      res.json({"msg": "El producto fue actualizado con éxito"})
+
+    }
+    else
+      res.status(404).json({"error": 404, "msg": "El producto que se intentó actualizar no existe"})
+  }
+  catch(err){
+    res.status(500).json({"error": 500, "msg": "Sucedió un error al intentar actualizar el producto"})
+  }
+}
 
 
+
+// Eliminando el producto
+const deleteProduct = async (req, res) => {
+  try{
+    const id = req.params.id;
+    // El producto que intento eliminar existe en la BD?
+    if(await db.Product.findByPk(id)){
+      // Elimino producto
+      db.Product.destroy({where: {id: id}})
+      // Elimino las imagenes asociadas al producto (de la BD)
+      db.Image.destroy({where: {productId: id}})
+      res.json({"msg": "El producto fue eliminado con éxisto"})
+    }
+    else
+      res.status(404).json({"error": 404, "msg": "El producto que se intentó eliminar no existe"})
+  }
+  catch(err){
+    res.status(500).json({"error": 500, "msg": "Sucedió un error al intentar eliminar el producto"})
+  }
+}
 
 module.exports = {
   findAllProduct,
   findById,
   findByCategory,
+  findByTag,
+  updateProduct,
+  deleteProduct
 }
--- sitio/source/routes/api/products.js
+++ sitio/source/routes/api/products.js
@@ -1,11 +1,22 @@
 const { Router } = require('express');
 const router = Router();
 
-const {findAllProduct, findById, findByCategory} = require('../../controllers/api/productController.js');
+const {findAllProduct, findById, findByCategory, findByTag, deleteProduct, updateProduct} = require('../../controllers/api/productController.js');
 
 // /api/products/
 router.get('/', findAllProduct)
+router.get('/search', findByTag)
 router.get('/detail/:id', findById)
 router.get('/:cat', findByCategory)
+
+// Creando producto
+
+// Actualizando
+router.put('/update/:id', updateProduct)
+// router.put('/update/images/:id', updateImage)
+
+// Eliminando
+router.delete('/delete/:id', deleteProduct)
+
 
 module.exports = router;
