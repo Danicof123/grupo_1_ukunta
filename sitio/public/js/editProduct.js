58e9be43c13d73b1c2f8fa74b09d5c5b598d8a30
Emanuel Romero <e.romero.1989@gmail.com>
2021-10-13 23:37:28 -0300

    Validación front de carga y edicion de productos

--- sitio/public/js/editProduct.js
+++ sitio/public/js/editProduct.js
@@ -3,5 +3,5 @@ import validationForm from './modules/validationForm.js';
 const d = document;
 
 d.addEventListener('DOMContentLoaded', (e) => {
-    validationForm('.form-addProduct'); //Envio la clase del formulario para ser evaluada
+    validationForm('.form-editProduct'); //Envio la clase del formulario para ser evaluada
 });
