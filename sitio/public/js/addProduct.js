5bcd6e5efaf0c99152485f891c64e3eb0ba27f4b
Emanuel Romero <e.romero.1989@gmail.com>
2021-10-15 20:17:09 -0300

    Merge con validacion de productos

--- sitio/public/js/addProduct.js
+++ sitio/public/js/addProduct.js
@@ -1,9 +1,7 @@
 import validationForm from './modules/validationForm.js';
-import dropMenu from './modules/drop-menu.js'
 
 const d = document;
 
 d.addEventListener('DOMContentLoaded', (e) => {
-    dropMenu(".btn-drop")
     validationForm('.form-addProduct'); //Envio la clase del formulario para ser evaluada
 });
