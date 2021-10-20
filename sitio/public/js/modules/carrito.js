8828379329150980dd3ee8edf5e75d1136014c4e
Daniel Fernandez <dani@bebot.design>
2021-10-19 23:55:19 -0300

    Carrito al 110%

--- sitio/public/js/modules/carrito.js
+++ sitio/public/js/modules/carrito.js
@@ -1,4 +1,4 @@
-import { obtenerProductos } from "./addProduct.js";
+import { cantidadProducto, obtenerProductos } from "./addProduct.js";
 import asuk from "./asuk.js";
 
 const d = document,
@@ -27,7 +27,34 @@ const carrito = () => {
       $carrito.classList.remove('active')
       $navbarBottom.classList.remove('active')
     }
+
+    // Boton eliminar producto individual del carrito ----------
+    if(e.target.matches(".btnEliminarProductoCarrito")){
+      // Elimino al producto del LocalStorage
+      localStorage.removeItem(e.target.dataset.id)
+
+      // Elimino al producto completo del carrito
+      e.target.parentNode.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode.parentNode)
+      
+      // Vuelvo a calcular el precio total
+      precioTotalCarrito();
+
+      // Actualizo los badges de cantidad
+      cantidadProductos();
+    }
   })
+}
+
+// Esta función calcula el precio total del carrito
+const precioTotalCarrito = () => {
+  const $precioTotal = d.querySelector('.price__carrito'),
+        $precioProducto = d.querySelectorAll('.carrito .carrito_precioProducto');
+  let acumulador = 0; //Variable temporal
+  // Me acumula el precio total
+  $precioProducto.forEach(pr => acumulador += Number(pr.textContent.slice(1)))
+
+  // Imprime el precio total
+  $precioTotal.textContent = `$${acumulador || "00,00"}`;
 }
 
 // Esta función es la que completa el carrito con los productos
@@ -50,18 +77,27 @@ const productCarrito = async () => {
   // Recorro y añado dinámicamente los productos al carrito
   productosByStorage.forEach(pr => {
     const $img = $template.querySelector('.carrito__imagenProducto');
+    const cantidad = Number(localStorage.getItem(pr.id));
     
     // Añadiendo informacion por cada producto
     $img.src = `/images/products/Bebidas/Cervezas/${pr.image[0].name}`;
     $img.alt = pr.image[0].name;
+    $template.querySelector('.carrito_titleProducto').textContent = pr.name;
+    $template.querySelector('.carrito_categoriaProducto').textContent = pr.category.name;
+    $template.querySelector('.carrito_cantidadProducto').textContent = `Cantidad: ${cantidad}`;
+    $template.querySelector('.carrito_precioProducto').textContent = `$${Number(pr.price) * cantidad}`
+    $template.querySelector('.btnEliminarProductoCarrito').dataset.id = pr.id;
     
     // Clonar el template, el true sirve para copiar toda la estructura
     const $clone = document.importNode($template, true);
     $fragment.appendChild($clone);
   });
- 
+  
   // El empleo de fragmentos es para pegarle al DOM una sola vez y no x c/producto que se agrega al carrito.
   $carrito.appendChild($fragment);
+
+  // Luego de mostrar los productos calculo el precio total
+  precioTotalCarrito();
 }
 
 // Lo llamo para q se actualicé en el primer momento los badges
