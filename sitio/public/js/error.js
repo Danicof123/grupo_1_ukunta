776fbd87fb72f02f943cd46630723d18dde6a16f
Daniel Fernandez <dani@bebot.design>
2021-10-23 20:00:45 -0300

    Observador y menuMobile en diferentes vistas

--- sitio/public/js/error.js
+++ sitio/public/js/error.js
@@ -1,10 +1,12 @@
 import './modules/isOld.js'
 import dropMenu from "./modules/drop-menu.js";
 import carrito from './modules/carrito.js';
+import { menuMobile } from './modules/menuMobile.js';
 
 ((c,d)=>{
     d.addEventListener('DOMContentLoaded', e => {
         dropMenu(".btn-drop") //Funcionalidad de menu desplegable al icono de usuario
+        menuMobile('.navbar__bars', '.navbar_ul') //Crea un menú dinámco con los navbar__item del menu principal
         carrito();
     })
 })(console, document);
--- sitio/public/js/store.js
+++ sitio/public/js/store.js
@@ -4,11 +4,13 @@ import carrito from './modules/carrito.js';
 import { carritoProduct } from './modules/addProduct.js';
 import { getProducts } from './modules/productsLoad.js';
 import scrollObserver from './modules/scrollObserver.js';
+import { menuMobile } from './modules/menuMobile.js';
 
 ((c,d,w)=>{
     d.addEventListener('DOMContentLoaded', e => {
         dropMenu(".btn-drop") //Funcionalidad de menu desplegable al icono de usuario
-        scrollObserver();
+        menuMobile('.navbar__bars', '.navbar_ul') //Crea un menú dinámco con los navbar__item del menu principal
+        scrollObserver(); //Una vez cargado todos los menu, se crea un observador para las secciones
         getProducts(); //Obtiene todos los productos
         carritoProduct(null, null, '.btnRel-add-carrito')
         carrito();
