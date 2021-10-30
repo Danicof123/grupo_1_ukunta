b3196f62b9540d034870f5eed53e1d9576228fec
Daniel Fernandez <dani@bebot.design>
2021-10-30 19:03:12 -0300

    API/design multiviews

--- sitio/public/js/modules/geoForm.js
+++ sitio/public/js/modules/geoForm.js
@@ -1,3 +1,4 @@
+import removeAccents from "../utils/removeAccents.js";
 import asuk from "./asuk.js";
 
 const d = document;
@@ -5,43 +6,106 @@ const d = document;
 // Carga el formulario
 const geoForm = ($form) => {
   const $selectAll = $form.querySelectorAll("select[data-geo]");
-        
+  // Recorre todos los select con data-geo, si el que se tiene que esperar no tiene valor, queda deshabilitado
   $selectAll.forEach((s) => {
     const $dataWait = s.dataset.geoWait;
-    ($dataWait) ? s.disabled = true : s.disabled = false;
+    if($dataWait)
+      ($form.querySelector(`select[data-geo="${$dataWait}"]`).value) 
+        ? s.disabled = false 
+        : s.disabled = true;
   });
+
+  return $form;
 };
 
-export const loadGeoSelect = ($select) => {
+
+export const loadGeoSelect = async ($select) => {
   const dataGeo = $select.dataset.geo,
         dataWait = $select.dataset.geoWait,
-        $form = $select.parentElement.parentElement;
+        $form = $select.parentElement.parentElement,
+        $fragment = d.createDocumentFragment();
 
   // Si el select tiene que esperar otro select...
   if(dataWait){
     const selectToWait = $form.querySelector(`select[data-geo="${dataWait}"]`);
+    const selectCountry = $form.querySelector(`select[data-geo="country"]`);
     
     // Si el select a esperar tiene algun valor, se procede a activar y rellenar segun los valores de la API correspondiente
     if(selectToWait.value){
       $select.disabled = false;
+      // Obtener API según corresponda
+      let geoAPI = apiCountry[selectCountry.value][dataGeo]
+      
+      // Si la API corresponde a un municipio, se agrega el valor del dataWait
+      if(dataGeo === "departamentos") geoAPI += `"${selectToWait.value}"&max=5000`
 
-      const $option = d.createElement('option');
+      // Traer los datos de la API
+      const dataAPI = await asuk.get(geoAPI);
+      
+      
+      // Ordenar alfabeticamente
+      const dataName = dataAPI[dataGeo].map(data => data.nombre).sort();
 
-      $option.value = "Prueba";
-      $option.text = "Pruebaaa";
+      // Añadir los nombres a los options de c/select
+      dataName.forEach(name => {
+        const $option = d.createElement('option');
+        $option.value = removeAccents(name);
+        $option.text = name;
 
-      $select.appendChild($option);
+        $fragment.appendChild($option)
+      })
+      $select.appendChild($fragment);
+      return $select;
     }
   }
 }
+
+
+export const changeGeoSelect = async ($form, $select, $value) => {
+  const selectCountry = $form.querySelector(`select[data-geo="country"]`),
+        dataGeo = $select.dataset.geo,
+        $option = d.createElement('option'),
+        $fragment = d.createDocumentFragment();
+
+  // Resetear Select     
+  $select.innerHTML = "";
+  $option.selected = true;
+  $option.disabled = true;
+  $option.text = `Elige una ${dataGeo === 'municipios' ? 'ciudad' : dataGeo}`;
+  $select.appendChild($option);
+
+  // Obtener API según corresponda
+  let geoAPI = apiCountry[selectCountry.value][dataGeo]
+        
+  // Si la API corresponde a un municipio, se agrega el valor del dataWait
+  if(dataGeo === "departamentos") geoAPI += `"${$value}"&max=1000`
+
+  // Traer los datos de la API
+  const dataAPI = await asuk.get(geoAPI);
+
+  // Ordenar alfabeticamente
+  const dataName = dataAPI[dataGeo].map(data => data.nombre).sort();
+  // Añadir los nombres a los options de c/select
+  dataName.forEach(name => {
+    const $option = d.createElement('option');
+    $option.value = removeAccents(name);
+    $option.text = name;
+
+    $fragment.appendChild($option)
+  })
+
+  $select.appendChild($fragment);
+  return $select;
+}
+
 
 
 
 // Tiene las Api de países/provincias/ciudades
 const apiCountry = {
   Argentina: {
-    province : "https://apis.datos.gob.ar/georef/api/provincias",
-    city: "https://apis.datos.gob.ar/georef/api/municipios?provincia=6"
+    provincias : "https://apis.datos.gob.ar/georef/api/provincias",
+    departamentos: "https://apis.datos.gob.ar/georef/api/departamentos?provincia="
   }
 };
 
--- sitio/public/js/modules/optProfile.js
+++ sitio/public/js/modules/optProfile.js
@@ -1,4 +1,4 @@
-import geoSelect, { loadGeoSelect } from "./geoForm.js";
+import geoSelect, { changeGeoSelect, loadGeoSelect } from "./geoForm.js";
 
 const d = document;
 let userProfile;
@@ -6,13 +6,14 @@ let userProfile;
 // Función principal del módulo optProfile---------------------------------------------------------
 export default function optProfile(pUserProfile) {
   userProfile = pUserProfile; //Almaceno el usuario que llega por parámetro en una variable global
-  console.log(userProfile);
+  console.log(userProfile); 
   // Obtengo todas las opciones
   let $opt = d.querySelectorAll(".profile__nav-row li");
 
   // Renderizo las opciones habilitadas
   renderOpt(optActive($opt));
 
+  // Evento Click ------------------------------------------------------------------------------------------------
   d.addEventListener("click", (e) => {
     // Cuando le hago click a un opt
     if (e.target.matches(".profile__nav-row li:not([disabled], .active)")) {
@@ -27,19 +28,25 @@ export default function optProfile(pUserProfile) {
     }
   });
 
-  // Cuando hay un evento change (Cambia algo en el formulario)--------------------
+  // Cuando hay un evento change (Cambia algo en el formulario)------------------------------------------------------
   d.addEventListener('change', e => {
     // Si el evento lo activa algún input que tenga el data-geo..
     if(e.target.dataset.geo){
+      const $form = e.target.parentElement.parentElement,
+            $selectToChange = $form.querySelector(`select[data-geo-wait="${e.target.dataset.geo}"]`)
       
+      // Al producirse un cambio en un select, se debe cambiar los valores del select dependiente
+      if($selectToChange)
+        changeGeoSelect($form, $selectToChange, e.target.value)
     }
   })
 }
 
-// Devuelve la opt en estado activo
-const optActive = () =>
-  d.querySelector(".profile__nav-row li.active").dataset.opt;
 
+// Devuelve la opt en estado activo----------------------------------------------------------------------//////
+const optActive = () => d.querySelector(".profile__nav-row li.active").dataset.opt;
+
+// Renderiza cada opt en estado activo----------------------------------------------------------------------//////
 const renderOpt = ($opt) => {
   const $profileOpt = d.querySelector(".profile__set-opt");
   $profileOpt.innerHTML = "";
@@ -93,27 +100,44 @@ const contact = () => {
 // ------------------------------------ OPT de address --------------------------------------------------------
 const address = () => {
   const $template = d.getElementById("setAddress").content,
-    $fragment = d.createDocumentFragment(),
-    userAddress = userProfile.address,
-    $form = $template.querySelector('form'),
-    sizeCountry = $template.querySelector('#country').length;
+        $form = $template.querySelector('form'),
+        $fragment = d.createDocumentFragment(),
+        userAddress = userProfile.address;
 
     // Llamada al geoSelect------------------
     geoSelect($form);
 
   // Recorro todas las direcciones que tiene guardada
-  userAddress.forEach((ad, i) => {
-    const $clone = d.importNode($template, true);
-    const $country = $clone.querySelectorAll('#country option');
-    const $SelectProvince = $clone.querySelector('#province');
+  userAddress.forEach((ad) => {
+    const $clone = d.importNode($template, true);  
+    // Select
+    const $country = $clone.querySelector('#country'),
+          $SelectProvince = $clone.querySelector('#provincias'),
+          $SelectCity = $clone.querySelector('#departamentos');
+    // Inputs Comunes
+    $clone.querySelector('#street').value = ad.street;
+    $clone.querySelector('#streetNumber').value = ad.streetNumber;
+    $clone.querySelector('#description').value = ad.description;
     
     // Recorre el select de países
-    for(let i = 0; i < sizeCountry; i++){
-      if($country[i].value === ad.country) $country[i].selected = true;
+    for(let i = 0; i < $country.options.length; i++){
+      if($country.options[i].value === ad.country) $country.options[i].selected = true;
     }
 
-    loadGeoSelect($SelectProvince)
-
+    loadGeoSelect($SelectProvince).then(prov => {
+      // Recorre el select de las provincias
+      for(let i = 0; i < prov.options.length; i++){
+        if(prov.options[i].value === ad.state)
+          prov.options[i].selected = true;
+      }
+      loadGeoSelect($SelectCity).then(city => {
+      // Recorre el select de las ciudades
+        for(let i = 0; i < city.options.length; i++){
+          if(city.options[i].value === ad.city)
+            city.options[i].selected = true;
+        }
+      })
+    })
 
     $fragment.appendChild($clone);
   });
