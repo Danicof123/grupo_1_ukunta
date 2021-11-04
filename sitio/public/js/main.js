50e0c91b2ab5751127317237cd0bae53d1d9a70d
Daniel Fernandez <dani@bebot.design>
2021-11-04 09:35:19 -0300

    Pills Alerts

--- sitio/public/js/main.js
+++ sitio/public/js/main.js
@@ -1,20 +1,8 @@
-import modal from "./components/modals.js";
+import { createAlert } from "./components/modals.js";
 import validationForm from "./modules/validationForm.js";
 
 ((d)=>{
     d.addEventListener('DOMContentLoaded', e => {    
         validationForm(".f-contact") //Envio la clase del formulario para ser evaluada
-        // modal({
-        //     message: "Esta es una ventana modal de prueba",
-        //     btnPrimary: "ACEPTAR",
-        //     btnSecondary: "CANCELAR"
-        // }, res => {
-        //     if(res){
-        //         console.log(res)
-        //     }
-        //     else{
-        //         console.log(res)
-        //     }
-        // })
     })
 })(document);
