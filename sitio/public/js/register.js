e67b927c5d2f54bae9c82d818042929a896c8e9b
B2DF <bernardinarte@gmail.com>
2021-11-04 01:41:21 -0300

    ver contraseña y eliminar campos de registro y validaciones

--- sitio/public/js/register.js
+++ sitio/public/js/register.js
@@ -5,3 +5,28 @@ const d = document;
 d.addEventListener('DOMContentLoaded', (e) => {
     validationForm('.form-register'); //Envio la clase del formulario para ser evaluada
 });
+
+d.addEventListener("DOMContentLoaded", function() {
+
+    
+    // icono para poder interaccionar
+    let showPassword = d.querySelector('.show-password');
+
+    showPassword.addEventListener('click', () => {
+      // elementos input de tipo password
+      let password1 = d.querySelector('.password1');
+      let password2 = d.querySelector('.password2');
+      
+
+      if ( password1.type === "text" ) {
+        password1.type = "password"
+        password2.type = "password"
+        
+        showPassword.classList.remove('fas fa-eye');
+      } else {
+        password1.type = "text"
+        password2.type = "text"
+        showPassword.classList.toggle("far fa-eye-slash");
+      }
+  })
+});
